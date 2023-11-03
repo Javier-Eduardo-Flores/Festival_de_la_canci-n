16029d9f4f67d41ef1f65a746ccb7433703e9a8b
Javier-Eduardo-Flores <javierfloresv30@gmail.com>
2023-11-03 17:29:41 -0600

    Pagina para el festival de la cancion

---
+++ src/main.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal(".header-images");
+ScrollReveal().reveal(".header-text" ,{dalay:500});
+ScrollReveal().reveal(".button" ,{dalay:500});
+ScrollReveal().reveal(".main-content" ,{dalay:500});
+ScrollReveal().reveal(".content" ,{dalay:500});
+ScrollReveal().reveal(".form-content" ,{dalay:500});
